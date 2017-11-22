6788a41968840f343cf049c37812d0b380b32f15
Varun Vasudevan <the.flazer@gmail.com>
2017-11-22 20:47:52 +0800

    Using eval for certain configs

--- server/config/index.js
+++ server/config/index.js
@@ -1,10 +1,19 @@
 const defaultConstants = require('./constants');
 const _ = require('lodash');
 
+const requireEval = [
+    'ENABLE_SEED',
+    'DEFAULT_PORT'
+];
+
 const getConstants = () => {
     return _.reduce(defaultConstants, (acc, v, k) => {
         if (_.includes(Object.keys(process.env), k)) {
-            acc[k] = process.env[k];
+            if (_.includes(requireEval, k)) {
+                acc[k] = eval(process.env[k]);
+            } else {
+                acc[k] = process.env[k];
+            }
         }
         else {
             acc[k] = v;
