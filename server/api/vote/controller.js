905dbdeeec1bb6078a1fcfe0a33d65d1f9086fbf
Varun Vasudevan <the.flazer@gmail.com>
2017-11-22 17:07:40 +0800

    Working /vote-counts

--- server/api/vote/controller.js
+++ server/api/vote/controller.js
@@ -12,8 +12,25 @@ module.exports = (cdb, redis) => {
     .then((result) => {
       return result;
     });
-  }
+  };
 
+  const generateNewScore = (currentScore, newScore) => {
+    let upvotes = currentScore.substring(0, currentScore.indexOf(','));
+    let downvotes = currentScore.substring(currentScore.indexOf(',') + 1, currentScore.length);
+    if (newScore == 1) {
+      upvotes ++;
+    } else if (newScore == -1) {
+      downvotes ++;
+    }
+    return `${upvotes},${downvotes}`;
+  };
+
+  const splitValue = (score) => {
+    return [
+      score.substring(0, score.indexOf(',')),
+      score.substring(score.indexOf(',') + 1, score.length),
+    ];
+  };
 
   router.post('/vote', (req, res, next) => {
     const score = parseInt(req.body.score, 10);
@@ -31,9 +48,8 @@ module.exports = (cdb, redis) => {
     .then((getResult) => {
       console.log('Length of list = ',topHundredLength);
       console.log('Current Score = ', getResult[0]);
-      const currentScore = getResult[0] == null ? 0 : getResult[0];
-      const newScore = currentScore + score;
-      const promises = [];
+      const currentScore = getResult[0] == null ? '0,0' : getResult[0];
+      const newScore = generateNewScore(currentScore, score);
       // update upvote/downvote of a given post - might need LRU for this
       return redis.set(postId, newScore).execAsync();
     })
@@ -57,17 +73,31 @@ module.exports = (cdb, redis) => {
   });
   
   router.get('/vote-counts', (req, res, next) => {
-    const postiveVotesQuery = `SELECT sum(score) FROM dev.votes WHERE postId=${req.query.postId} and score=1 ALLOW FILTERING`;
-    const negativeVotesQuery = `SELECT sum(score) FROM dev.votes WHERE postId=${req.query.postId} and score=-1 ALLOW FILTERING`;
-    return Promise.all([
-      cdb.execute(postiveVotesQuery, []),
-      cdb.execute(negativeVotesQuery, [])
-    ])
-    .spread((up, down) => {
-      console.log(`Endpoint called ${requestCounter++} times!`);
-      return res.status(200).send({
-        up,
-        down,
+    const postId = _.get(req, 'query.postId', null);
+      // try to get vote counts from Redis
+    return redis.get(postId).execAsync()
+    .then((val) => {
+      // if redis lookup is a hit, respond immediately
+      if (val[0] != null) {
+        const redisValues = splitValue(val[0]);
+        return res.status(200).send({
+          upvotes: redisValues[0],
+          downvotes: redisValues[1],
+        });
+      }
+      //if redisk lookup is not a hit, query from database
+      const postiveVotesQuery = `SELECT sum(score) FROM dev.votes WHERE postId=${req.query.postId} and score=1 ALLOW FILTERING`;
+      const negativeVotesQuery = `SELECT sum(score) FROM dev.votes WHERE postId=${req.query.postId} and score=-1 ALLOW FILTERING`;
+      return Promise.all([
+        cdb.execute(postiveVotesQuery, []),
+        cdb.execute(negativeVotesQuery, [])
+      ])
+      .spread((up, down) => {
+        console.log(`Endpoint called ${requestCounter++} times!`);
+        return res.status(200).send({
+          upvotes: up.rows[0]['system.sum(score)'],
+          downvotes: down.rows[0]['system.sum(score)'],
+        });
       });
     });
   });
@@ -77,9 +107,10 @@ module.exports = (cdb, redis) => {
     .then((range) => {
       // All redis results wrap the result in an array
       return res.status(200).send(_.map(range[0], (el) => {
+        const vals = splitValue(el);
         return {
-          postId: el.substring(0, el.indexOf(',')),
-          score: el.substring(el.indexOf(',') + 1, el.length),
+          postId: vals[0],
+          score: vals[1],
         };
       }));
     });
